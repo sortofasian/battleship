64d36f58a585eb28ddd6a794a0fd33779036f3aa
Mackenzie <a@a.a>
2024-07-30 10:20:02 -0400

    add search method

--- apps/backend/src/users/users.service.ts
+++ apps/backend/src/users/users.service.ts
@@ -2,6 +2,8 @@ import { Injectable } from "@nestjs/common"
 import { User } from "@prisma/client"
 
 import { DbService } from "../db/db.service"
+import {DbModule} from "../db/db.module"
+import {startWith} from "rxjs"
 
 @Injectable()
 export class UsersService {
@@ -9,8 +11,16 @@ export class UsersService {
 
     async search(query: string): Promise<User[]> {
         // Return a list of users by partial username
+        return this.db.user.findMany(
+            {
+                where: {
+                    username: {
+                        startsWith: query
+                    }
+                }
+            }
+        )
     }
-
     async read(id: string): Promise<User> {
         // Return one user by id
         return this.db.user.findUnique({
